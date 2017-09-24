ec8b96c2db128ddc6546de3be575b91d03ad64d8
Alexander <Lynx@Alexanders-MacBook-Pro.local>
2017-09-24 21:20:02 +0300

    del comment

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ import Tournament from './RouteHandlers/Tournament'
 
 export default (
     <Router history={browserHistory}>
-        <Route path='/ids/:sport' component={Tournaments}>  {/*это может быть меню из tournaments со списком всех торнаментов*/}
+        <Route path='/ids/:sport' component={Tournaments}>
             <Route path='tournament/:tournamentId' component={Tournament} />
         </Route>
     </Router>
